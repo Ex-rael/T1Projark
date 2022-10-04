911e2b54e781a28892d7b69cea9c0c42b28bd15b
Israel Garcia <i.graziola@edu.pucrs.br>
2022-10-04 01:06:52 +0000

    commit 1

---
+++ trab/src/test/java/com/projark/trab/TrabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projark.trab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
