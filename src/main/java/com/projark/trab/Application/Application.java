3923c872d6fc6421179bef53a74ad49e5f572589
felipeffagundes <f.fagundes001@edu.pucrs.br>
2022-10-18 19:56:51 -0300

    mais alterações no arquivo application

--- src/main/java/com/projark/trab/Application/Application.java
+++ src/main/java/com/projark/trab/Application/Application.java
@@ -1,4 +1,4 @@
-package com.projark.trab.Controllers;
+package com.projark.trab.Application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
