d9cc571375851dffe5c46321507912bd8176b87c
felipeffagundes <f.fagundes001@edu.pucrs.br>
2022-10-18 18:38:36 -0300

    corrigindo diretorio do arquivo application

--- src/main/java/com/projark/trab/Application.java
+++ src/main/java/com/projark/trab/Application.java
@@ -1,4 +1,4 @@
-package com.projark.trab.Controller;
+package com.projark.trab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/projark/trab/Controller/HelloController.java
+++ src/main/java/com/projark/trab/Controller/HelloController.java
@@ -10,6 +10,6 @@ public class HelloController {
     @RequestMapping("/")
     @ResponseBody
     public String hello(){
-        return "papi copstein";
+        return "quem leu dá o boga";
     }
 }
