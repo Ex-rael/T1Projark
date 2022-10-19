16d4b366da07ba830fbc5e49091418f30f505bad
felipeffagundes <f.fagundes001@edu.pucrs.br>
2022-10-19 20:47:31 -0300

    adição do link do repo no comentário de Application.java

--- src/main/java/com/projark/trab/Controllers/Application.java
+++ src/main/java/com/projark/trab/Controllers/Application.java
@@ -1,4 +1,5 @@
 // T1 de Projeto e Arquitetura de Software - Arthur Zanella Lovato, Felipe Fonseca Fagundes, Israel Graziola Garcia, Luiz Felipe Porto Lara de Oliveira
+// github.com/Ex-rael/T1Projark
 package com.projark.trab.Controllers;
 
 import org.slf4j.Logger;
