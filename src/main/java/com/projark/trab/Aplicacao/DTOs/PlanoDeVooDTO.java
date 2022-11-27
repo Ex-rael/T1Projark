d15422625323c2f5daecfa28d064fdf7805bb199
Felipe <ffagundes1234@gmail.com>
2022-11-27 16:09:47 -0300

    arrumando DTOs

--- src/main/java/com/projark/trab/Aplicacao/DTOs/PlanoDeVooDTO.java
+++ src/main/java/com/projark/trab/Aplicacao/DTOs/PlanoDeVooDTO.java
@@ -1,5 +1,70 @@
 package com.projark.trab.Aplicacao.DTOs;
 
-public class PlanoDeVooDTO {
+import java.time.LocalDateTime;
+import java.util.Objects;
 
+import com.projark.trab.Dominio.Entidades.Rota;
+
+public class PlanoDeVooDTO {
+    private LocalDateTime data;
+    private double altitude;
+    private Rota rota;
+    private double velCruzeiro;
+    
+    public PlanoDeVooDTO(LocalDateTime data, double altitude, Rota rota, double velCruzeiro) {
+        this.data = data;
+        this.altitude = altitude;
+        this.rota = rota;
+        this.velCruzeiro = velCruzeiro;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
+
+    public void setAltitude(double altitude) {
+        this.altitude = altitude;
+    }
+
+    public void setRota(Rota rota) {
+        this.rota = rota;
+    }
+
+    public void setVelCruzeiro(double velCruzeiro) {
+        this.velCruzeiro = velCruzeiro;
+    }
+
+    public double getAltitude() {
+        return altitude;
+    }
+
+    public Rota getRota() {
+        return rota;
+    }
+
+    public double getVelCruzeiro() {
+        return velCruzeiro;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(data, altitude, rota, velCruzeiro);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!(obj instanceof PlanoDeVooDTO))
+            return false;
+        PlanoDeVooDTO other = (PlanoDeVooDTO) obj;
+        return Objects.equals(data, other.data)
+                && Double.doubleToLongBits(altitude) == Double.doubleToLongBits(other.altitude)
+                && Objects.equals(rota, other.rota)
+                && Double.doubleToLongBits(velCruzeiro) == Double.doubleToLongBits(other.velCruzeiro);
+    }
 }
--- src/main/java/com/projark/trab/Aplicacao/DTOs/SlotsDTO.java
+++ src/main/java/com/projark/trab/Aplicacao/DTOs/SlotsDTO.java
@@ -1,5 +1,34 @@
 package com.projark.trab.Aplicacao.DTOs;
 
-public class SlotsDTO {
+import java.util.Arrays;
 
+public class SlotsDTO {
+    private int duracaoEmMinutos;
+    private int[] altitudes;
+
+    public SlotsDTO(int duracaoEmMinutos, int[] altitudes) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+        this.altitudes = altitudes;
+    }
+
+    public int getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+    public int[] getAltitudes() {
+        return altitudes;
+    }
+
+    public void setAltitudes(int[] altitudes) {
+        this.altitudes = altitudes;
+    }
+
+    @Override
+    public String toString() {
+        return "Slots [duracaoEmMinutos=" + duracaoEmMinutos + ", altitudes=" + Arrays.toString(altitudes) + "]";
+    }
 }
